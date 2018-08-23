d03deace1462c6c216a94f4e5a4cff96b176108a
Danry <danryw@iastate.edu>
2018-08-23 09:45:08 -0500

    work on hw0

--- hw0/src/hw0.c
+++ hw0/src/hw0.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define N 5
 
-int main(void) {
-	puts("Hello World!!!"); /* prints Hello World!!! */
-	return EXIT_SUCCESS;
+int main(void)
+{
+  int board[N][N];
+
+  /* initializing the board */
+  int number = 1;
+  for (int row=0; row<N; row++)
+    for (int col=0; col<N; col++)
+      board[row][col] = number++;
+
+  /*
+    move from top-left in clockwise direction
+    move   1   2   3   4   5   6   7   8
+    row   -2  -2  -1  +1  +2  +2  +1  -1
+    col   -1  +1  +2  +2  +1  -1  -2  -2
+   */
+
+
+  return EXIT_SUCCESS;
 }

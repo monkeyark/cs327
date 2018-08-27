bcf6e48ea145439a1535efe692b02c2257d9aaf2
Danry <danryw@iastate.edu>
2018-08-27 12:27:44 -0500

    work on hw0

--- hw0/src/hw0.c
+++ hw0/src/hw0.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "stdbool.h"
 #define N 5
 
 /*
@@ -22,44 +23,56 @@ And you'll need to do a little bit of bookkeeping to record your move orders so 
 
 int main()
 {
-  initialBoard();
-  for (int i=0; i<N; i++)
-    for (int j=0; j<N; j++)
-      move(i, j);
-  /*
-    move from top-left in clockwise direction
-    move   1   2   3   4   5   6   7   8
-    row   -2  -2  -1  +1  +2  +2  +1  -1
-    col   -1  +1  +2  +2  +1  -1  -2  -2
-   */
-  
-  return 0;
-}
-
-int move(int row, int col)
-{
-  /*
-    move from top-left in clockwise direction
-    move   1   2   3   4   5   6   7   8
-    row   -2  -2  -1  +1  +2  +2  +1  -1
-    col   -1  +1  +2  +2  +1  -1  -2  -2
-   */
-
-  int rowMove[8] = {-2,  -2,  -1,  +1,  +2,  +2,  +1,  -1};
-  int colMove[8] = {-1,  +1,  +2,  +2,  +1,  -1,  -2,  -2};
-  
-  return 0;
-}
-
-
-void initialBoard()
-{
+  /* initializing the board */
   int board[N][N];
 
-  /* initializing the board */
   int number = 1;
   for (int row=0; row<N; row++)
     for (int col=0; col<N; col++)
       board[row][col] = number++;
+
+  /* initializing the board record */
+  bool record[N][N];
+  for (int i=0; i<N; i++)
+    for (int j=0; j<N; j++)
+    	record[i][j] = false;
+
+  /* for each block on board start tour */
+  for (int i=0; i<N; i++)
+    for (int j=0; j<N; j++)
+      move(i, j);
+
+  /*
+    move from top-left in clockwise direction
+    move   1   2   3   4   5   6   7   8
+    row   -2  -2  -1  +1  +2  +2  +1  -1
+    col   -1  +1  +2  +2  +1  -1  -2  -2
+   */
+
+  return 0;
+}
+
+bool move(int row, int col)
+{
+  /*
+    move from top-left in clockwise direction
+    move   1   2   3   4   5   6   7   8
+    row   -2  -2  -1  +1  +2  +2  +1  -1
+    col   -1  +1  +2  +2  +1  -1  -2  -2
+   */
+  int rowMove[8] = {-2,  -2,  -1,  +1,  +2,  +2,  +1,  -1};
+  int colMove[8] = {-1,  +1,  +2,  +2,  +1,  -1,  -2,  -2};
+  
+  return true;
+}
+
+
+bool recordBoard(bool record[N][N], int row, int col)
+{
+  if (record[N][N])
+	  return false;
+  else
+
+  return true;
 }
 

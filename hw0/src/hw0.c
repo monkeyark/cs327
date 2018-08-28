a554bf9b3e56b5916ebff9b191024eae31714821
Danry <danryw@iastate.edu>
2018-08-28 02:27:46 -0500

    work on hw0

--- hw0/src/hw0.c
+++ hw0/src/hw0.c
@@ -6,19 +6,26 @@
 int board[N][N];
 bool isVisit[N][N];
 int solu[N*N];
+int numSolu = 0;
 
-bool isMove(int row, int col, int tour)
+bool isMove(int row, int col, int step)
 {
 	return row >= 0 && row < N
 		&& col >= 0 && col < N
 		&& !isVisit[row][col]
-		&& tour < N*N;
+		&& step < N*N;
 }
 
-int soluPath(int solu[])
+void printSolu(int solu[])
 {
-	//TODO
-	return 0;
+	//for (int i=0; i<(sizeof(solu)/sizeof(solu[0])); i++)
+
+	for (int i=0; i<N*N; i++)
+	{
+		printf("%d ", solu[i]);
+	}
+	printf("\n");
+	return;
 }
 
 /*
@@ -27,58 +34,39 @@ move       0   1   2   3   4   5   6   7
 row       -2  -2  -1  +1  +2  +2  +1  -1
 col       -1  +1  +2  +2  +1  -1  -2  -2
 */
-bool knightTour(int row, int col, int move, int tour)
+bool knightTour(int row, int col, int step)
 {
 	int rowMove[8] = {-2,  -2,  -1,  +1,  +2,  +2,  +1,  -1};
 	int colMove[8] = {-1,  +1,  +2,  +2,  +1,  -1,  -2,  -2};
 
 	isVisit[row][col] = true;
 
-	if (tour == N*N-1) return true;
+	if (step == N*N-1) return true;
 
 	for (int i=0; i<8; i++)
 	{
 		row += rowMove[i];
 		col += colMove[i];
-		tour++;
 
-		if (isMove(row, col, tour))
+		if (isMove(row, col, step))
 		{
-			if (knightTour(row, col, i, tour))
+			//add to soluPath
+			solu[step] = board[row][col];
+
+			if (knightTour(row, col, step))
 			{
-				solu[tour] = board[row][col];
-				//TODO add to soluPath
+				numSolu++;
+				printSolu(solu);
 				return true;
 			}
-			//TODO backtrack
-			row -= rowMove[i];
-			col -= colMove[i];
-
-//			knightTour(row, col, move++, tour--);
+			//backtrack
+			solu[step--] = 0;
 		}
 	}
 
 	isVisit[row][col] = false;
 	return false;
 }
-
-/*
-main()
-{
-  For each square s
-    tour(s)
-}
-
-tour(start)
-{
-  mark start as visited
-  for each unmarked square s reachable by a knight's move from start
-    tour(s)
-  unmark start
-}
-
-And you'll need to do a little bit of bookkeeping to record your move orders so that you can print.
-*/
 
 int main()
 {
@@ -105,12 +93,11 @@ int main()
 	{
 		for (int col=0; col<N; col++)
 		{
-			knightTour(row, col, 0, 0);
+			knightTour(row, col, 0);
 		}
 	}
 
-	//TODO print(soluPath)
-
+	printf("\ntotal solution: %d\n", numSolu);
 
 	return 0;
 }

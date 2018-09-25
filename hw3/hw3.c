87208a947e0de6be8417123a43a1116d74c88056
monkeyark <danryw@outlook.com>
2018-09-24 20:34:17 -0500

    add dijkstra

--- hw3/hw3.c
+++ hw3/hw3.c
@@ -464,6 +464,120 @@ void saveFile(FILE *f)
 	fclose(f);
 }
 
+static void dijkstra(dungeon_t *d, pair_t from, pair_t to)
+{
+	static corridor_path_t path[DUNGEON_Y][DUNGEON_X], *p;
+	static uint32_t initialized = 0;
+	heap_t h;
+	uint32_t x, y;
+
+	if (!initialized)
+	{
+		for (y = 0; y < DUNGEON_Y; y++)
+		{
+			for (x = 0; x < DUNGEON_X; x++)
+			{
+				path[y][x].pos[dim_y] = y;
+				path[y][x].pos[dim_x] = x;
+			}
+		}
+		initialized = 1;
+	}
+
+	for (y = 0; y < DUNGEON_Y; y++)
+	{
+		for (x = 0; x < DUNGEON_X; x++)
+		{
+			path[y][x].cost = INT_MAX;
+		}
+	}
+
+	path[from[dim_y]][from[dim_x]].cost = 0;
+
+	heap_init(&h, corridor_path_cmp, NULL);
+
+	for (y = 0; y < DUNGEON_Y; y++)
+	{
+		for (x = 0; x < DUNGEON_X; x++)
+		{
+			if (mapxy(x, y) != ter_wall_immutable)
+			{
+				path[y][x].hn = heap_insert(&h, &path[y][x]);
+			}
+			else
+			{
+				path[y][x].hn = NULL;
+			}
+		}
+	}
+
+	while ((p = heap_remove_min(&h)))
+	{
+		p->hn = NULL;
+
+		if ((p->pos[dim_y] == to[dim_y]) && p->pos[dim_x] == to[dim_x])
+		{
+			for (x = to[dim_x], y = to[dim_y];
+					(x != from[dim_x]) || (y != from[dim_y]);
+					p = &path[y][x], x = p->from[dim_x], y = p->from[dim_y])
+			{
+				if (mapxy(x, y) != ter_floor_room)
+				{
+					mapxy(x, y) = ter_floor_hall;
+					hardnessxy(x, y) = 0;
+				}
+			}
+			heap_delete(&h);
+			return;
+		}
+
+		if ((path[p->pos[dim_y] - 1] [p->pos[dim_x]].hn) &&
+				(path[p->pos[dim_y] - 1][p->pos[dim_x]].cost >
+				 p->cost + hardnesspair(p->pos)))
+		{
+			path[p->pos[dim_y] - 1][p->pos[dim_x]].cost =
+				p->cost + hardnesspair(p->pos);
+			path[p->pos[dim_y] - 1][p->pos[dim_x]].from[dim_y] = p->pos[dim_y];
+			path[p->pos[dim_y] - 1][p->pos[dim_x]].from[dim_x] = p->pos[dim_x];
+			heap_decrease_key_no_replace(&h, path[p->pos[dim_y] - 1]
+					[p->pos[dim_x]].hn);
+		}
+		if ((path[p->pos[dim_y]][p->pos[dim_x] - 1].hn) &&
+				(path[p->pos[dim_y]][p->pos[dim_x] - 1].cost >
+				 p->cost + hardnesspair(p->pos)))
+		{
+			path[p->pos[dim_y]][p->pos[dim_x] - 1].cost =
+				p->cost + hardnesspair(p->pos);
+			path[p->pos[dim_y]][p->pos[dim_x] - 1].from[dim_y] = p->pos[dim_y];
+			path[p->pos[dim_y]][p->pos[dim_x] - 1].from[dim_x] = p->pos[dim_x];
+			heap_decrease_key_no_replace(&h, path[p->pos[dim_y]]
+					[p->pos[dim_x] - 1].hn);
+		}
+		if ((path[p->pos[dim_y]][p->pos[dim_x] + 1].hn) &&
+				(path[p->pos[dim_y]][p->pos[dim_x] + 1].cost >
+				 p->cost + hardnesspair(p->pos)))
+		{
+			path[p->pos[dim_y]][p->pos[dim_x] + 1].cost =
+				p->cost + hardnesspair(p->pos);
+			path[p->pos[dim_y]][p->pos[dim_x] + 1].from[dim_y] = p->pos[dim_y];
+			path[p->pos[dim_y]][p->pos[dim_x] + 1].from[dim_x] = p->pos[dim_x];
+			heap_decrease_key_no_replace(&h, path[p->pos[dim_y]]
+					[p->pos[dim_x] + 1].hn);
+		}
+		if ((path[p->pos[dim_y] + 1][p->pos[dim_x]].hn) &&
+				(path[p->pos[dim_y] + 1][p->pos[dim_x]].cost >
+				 p->cost + hardnesspair(p->pos)))
+		{
+			path[p->pos[dim_y] + 1][p->pos[dim_x]].cost =
+				p->cost + hardnesspair(p->pos);
+			path[p->pos[dim_y] + 1][p->pos[dim_x]].from[dim_y] = p->pos[dim_y];
+			path[p->pos[dim_y] + 1][p->pos[dim_x]].from[dim_x] = p->pos[dim_x];
+			heap_decrease_key_no_replace(&h, path[p->pos[dim_y] + 1]
+					[p->pos[dim_x]].hn);
+		}
+	}
+}
+
 int main(int argc, char *argv[])
 {
 	char *home = getenv("HOME");

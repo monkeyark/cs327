b69e6d13195c188136a399d2d2007b0ac93e01d2
monkeyark <danryw@outlook.com>
2018-10-23 16:00:22 -0500

    update header

--- hw5/queue.h
+++ hw5/queue.h
@@ -5,13 +5,12 @@
 #include <stdint.h>
 
 
-struct queue_node;
-
 typedef struct queue_node Node;
 
 struct queue_node
 {
 	Node *next;
+    Node *prev;
 	int priority;
 };
 
@@ -35,34 +34,4 @@ bool pq_isEmpty(Queue pq, Node **head);
 void pq_delete();
 
 
-/*
-struct queue_node_npc;
-
-typedef struct queue_node_npc Node_t;
-
-struct queue_node_npc
-{
-	Node_t *next;
-	int priority;
-	Character character;
-};
-
-typedef struct Priority_Queue_npc
-{
-  Node_t **head;
-  uint32_t size;
-  int32_t (*compare)(const void *key, const void *with);
-  void (*datum_delete)(void *);
-} Queue_npc;
-
-
-
-Node_t *node_new_NPC(int priority, Character c);
-
-void pq_insert_NPC(Queue_npc pq, Node_t** head, Node_t** new);
-
-Node_t *pq_pop_NPC(Queue_npc pq, Node_t **head);
-
-void pq_delete_NPC(Queue_npc pq);
-*/
 #endif

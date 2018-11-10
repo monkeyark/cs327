df6203c653a20ff507b4941d776f7c070ac878a3
monkeyark <danryw@outlook.com>
2018-11-09 23:00:51 -0600

    BUG BOUNTY

--- hw_solu/rlg327-f2018-1.07/descriptions.cpp
+++ hw_solu/rlg327-f2018-1.07/descriptions.cpp
@@ -56,7 +56,7 @@ static const struct {
   /* We can use this convenient macro here, but we can't use a *
    * similar macro above because of PASS and TELE.             */
   /* color_lu_entry(BLACK), Can't display COLOR_BLACK */
-  "BLACK", COLOR_WHITE,
+  "BLACK", COLOR_WHITE, //BUG is here
   color_lu_entry(BLUE),
   color_lu_entry(CYAN),
   color_lu_entry(GREEN),
